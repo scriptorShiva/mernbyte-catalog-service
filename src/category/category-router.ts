2cb96d741bda9bee617242f3490be13a6447f270
scriptorShiva <shivapal108941@gmail.com>
2025-07-12 13:28:42 +0530

    Admin authorization to create category

--- src/category/category-router.ts
+++ src/category/category-router.ts
@@ -5,6 +5,8 @@ import { CategoryService } from './category-service';
 import Logger from '../config/logger';
 import { asyncErrorHandlerWrapper } from '../common/utils/asyncErrorHandlerWrapper';
 import authenticate from '../common/middlewares/authenticate';
+import { canAccess } from '../common/middlewares/canAccess';
+import { ROLES } from '../common/constant';
 
 const router = express.Router();
 
@@ -16,6 +18,7 @@ const categoryController = new CategoryController(categoryService, Logger);
 router.post(
     '/',
     authenticate,
+    canAccess([ROLES.ADMIN]),
     categoryValidator,
     asyncErrorHandlerWrapper(categoryController.create),
 );
--- src/common/types/index.ts
+++ src/common/types/index.ts
@@ -1,4 +1,14 @@
+import { Request } from 'express';
+
 export type AuthCookie = {
     accessToken: string;
     refreshToken: string;
 };
+
+export interface AuthRequest extends Request {
+    auth: {
+        sub: string;
+        role: string;
+        id?: string;
+    };
+}
